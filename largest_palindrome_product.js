79e78a51c29a4368bb9d17fb560f5b8a83f80b65
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-02-17 13:33:01 -1000

    right first tryyyyyy

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,18 +5,51 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+module.exports = function(digits) {
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
 
   // do your work here
+  const isPalind = num => {
+    let numStr = num + "";
+    let splitStr = numStr.split("");
+    splitStr.reverse();
+    splitStr = splitStr.join("");
+    splitStr *= 1;
+    return num === splitStr;
+  };
 
+  const findBigPal = digs => {
+    let digArr = [];
+    for (let d = 0; d < digs; d++) {
+      digArr.push(9);
+    }
+    let maxFactor = digArr.join("");
 
+    for (let i = 0; i <= maxFactor; i++) {
+      for (let j = 0; j <= maxFactor; j++) {
+        if (i * j > palindromeNumber && isPalind(i * j)) {
+          factor_0 = i;
+          factor_1 = j;
+          palindromeNumber = i * j;
+          // console.log(
+          //   "f0: ",
+          //   factor_0,
+          //   " f1: ",
+          //   factor_1,
+          //   " p: ",
+          //   palindromeNumber
+          // );
+        }
+      }
+    }
+  };
 
+  findBigPal(digits);
   return {
-    factor_0 : factor_0,
-    factor_1 : factor_1,
-    palindromeNumber : palindromeNumber
+    factor_0: factor_0,
+    factor_1: factor_1,
+    palindromeNumber: palindromeNumber
   };
 };
